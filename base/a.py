ab3684702efc4a1d40d4f26bda87921657a267a2
3xian <suffixtree@gmail.com>
2013-07-10 22:52:31 +0800

    .

--- base/a.py
+++ base/a.py
@@ -1,5 +1,6 @@
 from collections import Counter
 from itertools import cycle, product as prod, permutations as perm, combinations as comb, combinations_with_replacement as combr
+from sys import stdin, stdout
 read_ints = lambda: map(int, raw_input().split())
 read_floats = lambda: map(float, raw_input().split())
 
