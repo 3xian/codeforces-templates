b7528fef74d6cf71395212936d435d30f11e3c2b
3xian <daiwei@liyanhong.(none)>
2013-07-10 13:15:33 +0800

    .

---
+++ base/a.py
@@ -0,0 +1,10 @@
+from collections import Counter
+from itertools import cycle, product as prod, permutations as perm, combinations as comb, combinations_with_replacement as combr
+read_ints = lambda: map(int, raw_input().split())
+read_floats = lambda: map(float, raw_input().split())
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
