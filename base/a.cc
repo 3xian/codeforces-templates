3e559c8d87276da400c35434b231f5c27c278fbf
Wei Dai <suffixtree@gmail.com>
2013-07-19 23:30:29 +0800

    Update a.cc

--- base/a.cc
+++ base/a.cc
@@ -3,13 +3,14 @@
 #include <cstdio>
 #include <cstring>
 #include <algorithm>
+#include <iomanip>
 #include <iostream>
+#include <limits>
 #include <map>
 #include <queue>
 #include <set>
 #include <sstream>
 #include <vector>
-#define all(x) (x).begin(), (x).end()
 #define range(i, n) for (int i=0; i<(n); ++i)
 #ifdef ONLINE_JUDGE
 #define logging(x)
@@ -22,10 +23,10 @@ using namespace std;
 int d4[4][2] = {{1,0}, {0,1}, {-1,0}, {0,-1}};
 tpl_t void lmax(T& a, const T& b) { if (a < b) a = b; }
 tpl_t void lmin(T& a, const T& b) { if (a > b) a = b; }
-tpl_t void usort(vector<T>& a) { sort(all(a)); a.erase(unique(all(a)),a.end()); }
+tpl_t void usort(vector<T>& a) { sort(all(a)); a.erase(unique(a.begin(),a.end()),a.end()); }
 tpl_t void operator +=(vector<T>& v, const T& x) { v.push_back(x); }
 string str(int i) { char s[16]; sprintf(s,"%d",i); return s; }
-//...ooo000...ooo000...ooo000...ooo000...ooo000...ooo000...ooo000...ooo000...ooo000
+//...ooo000...ooo000...ooo000...ooo000...ooo000...ooo000...ooo000...ooo000...ooo000...ooo000
 
 int main() {
 	ios::sync_with_stdio(false);
