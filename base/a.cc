5b7519f30d1c4cc8dc2d0f5c438adda1dd30dfce
Wei Dai <suffixtree@gmail.com>
2013-11-15 14:55:33 +0800

    Update a.cc

--- base/a.cc
+++ base/a.cc
@@ -1,22 +1,15 @@
 #include <bits/stdc++.h>
-#define range(i, n) for (int i=0; i<(n); ++i)
 #ifdef ONLINE_JUDGE
 #define logging(x)
 #else
-#define logging(x) cerr << #x":" << x << endl
+#define logging(x) cout<<"\033[31m ---> "#x" = "<<x<<'\n'
 #endif
-#define TP template<typename T>
-#define stream(a) string _##a; getline(cin, _##a); stringstream a(_##a);
 using namespace std;
-int d4[4][2] = {{1,0}, {0,1}, {-1,0}, {0,-1}};
-TP void lmax(T& a, const T& b) { if (a < b) a = b; }
-TP void lmin(T& a, const T& b) { if (a > b) a = b; }
-TP void usort(vector<T>& a) { sort(a.begin(),a.end()); a.erase(unique(a.begin(),a.end()), a.end()); }
-TP void operator +=(vector<T>& v, const T& x) { v.push_back(x); }
-string str(int i) { char s[16]; sprintf(s,"%d",i); return s; }
-//...ooo000...ooo000...ooo000...ooo000...ooo000...ooo000...ooo000...ooo000...ooo000...ooo000
+struct SpeedUp{ SpeedUp() { ios::sync_with_stdio(false); cin.tie(NULL); } } _;
+string ToString(int i) { char s[16]; sprintf(s, "%d", i); return s; };
+template<typename T>
+void Unique(vector<T>& a) { sort(a.begin(), a.end()); a.erase(unique(a.begin(), a.end()), a.end()); };
 
 int main() {
-    ios::sync_with_stdio(false);
-    return 0;
+  return 0;
 }
