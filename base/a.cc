cee3f94eb245cdb43ee07892587d34030261cd36
Wei Dai <suffixtree@gmail.com>
2014-05-31 04:09:02 +0800

    Update a.cc

--- base/a.cc
+++ base/a.cc
@@ -1,16 +1,14 @@
 #include <bits/stdc++.h>
-#include <ext/rope>
-#ifdef ONLINE_JUDGE
-#define LOG(x)
-#else
-#define LOG(x) cerr<<"\033[34m"#x" => \033[32m"<<x<<"\033[0m\n"
-#endif
 using namespace std;
-using namespace __gnu_cxx;
-struct SpeedUp{SpeedUp(){ios::sync_with_stdio(false);cin.tie(NULL);}}_;
-string ToString(int i) {char s[16];sprintf(s,"%d",i);return s;};
-template<typename T>void Unique(vector<T>& a){sort(a.begin(),a.end());a.erase(unique(a.begin(),a.end()),a.end());};
+void io_trick() { ios::sync_with_stdio(false); cin.tie(NULL); }
+string itos(int value) { char buf[12]; sprintf(buf, "%d", value); return buf; }
+template<class T> void sort(vector<T>& a){ sort(a.begin(), a.end()); }
+template<class T> void uniq(vector<T>& a){ sort(a); a.erase(unique(a.begin(),a.end()),a.end()); }
 
 int main() {
+    io_trick();
+
+    // Start here
+
     return 0;
 }
