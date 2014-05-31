b7a9f718b68336ae50102c7412dd853d2e544ce5
Wei Dai <suffixtree@gmail.com>
2014-05-31 12:22:26 +0800

    Update a.cc

--- base/a.cc
+++ base/a.cc
@@ -1,13 +1,11 @@
 #include <bits/stdc++.h>
+#define vec std::vector
+#define whole(a) (a).begin(),(a).end()
 using namespace std;
-#define M std::map
-#define S std::set
-#define V std::vector
-#define ALL(a) (a).begin(),(a).end()
-void setio() { ios::sync_with_stdio(false); cin.tie(NULL); }
+void setio() { ios::sync_with_stdio(0); cin.tie(0); }
 string itos(int value) { char buf[12]; sprintf(buf, "%d", value); return buf; }
-template<class T> void uniq(V<T>& a){ a.erase(unique(ALL(a)), a.end()); }
-template<class T> void operator +=(V<T> &a, const T& x){ a.push_back(x); }
+template<class T> void uniq(vec<T>& a){ sort(whole(a)); a.erase(unique(whole(a)), a.end()); }
+template<class T> void operator +=(vec<T> &a, const T& x){ a.push_back(x); }
 
 int main() {
     setio();
